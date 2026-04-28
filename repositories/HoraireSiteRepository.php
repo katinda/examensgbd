59f42deb2ac4af30e6fe3ccac0b165e925909545
katinda <katindamangala@gmail.com>
2026-04-28 12:29:03 +0200

    Ajout HoraireSiteRepository

---
+++ repositories/HoraireSiteRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/../models/HoraireSite.php';
+
+class HoraireSiteRepository {
+
+    public function __construct(private PDO $pdo) {}
+
+    public function delete(int $id): void {
+        $stmt = $this->pdo->prepare("DELETE FROM Horaires_Sites WHERE Horaire_ID = :id");
+        $stmt->execute([':id' => $id]);
+    }
+
+    private function hydrateOne(array $row): HoraireSite {
+        return new HoraireSite(
+            (int) $row['Horaire_ID'],
+            (int) $row['Site_ID'],
+            (int) $row['Annee'],
+            $row['Heure_Debut'],
+            $row['Heure_Fin']
+        );
+    }
+}
