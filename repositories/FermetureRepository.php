4eb3d25b0fae9b84ca90f15abad54ddf83d0de5f
katinda <katindamangala@gmail.com>
2026-04-28 14:24:13 +0200

    Ajout FermetureRepository

---
+++ repositories/FermetureRepository.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../models/Fermeture.php';
+class FermetureRepository {
+    public function __construct(private PDO $pdo) {}
+    public function findGlobales(): array { $stmt = $this->pdo->query("SELECT * FROM Fermetures WHERE Site_ID IS NULL"); return array_map(fn($row) => $this->hydrateOne($row), $stmt->fetchAll(PDO::FETCH_ASSOC)); }
+    private function hydrateOne(array $row): Fermeture {
+        return new Fermeture((int) $row['Fermeture_ID'], $row['Site_ID'] !== null ? (int) $row['Site_ID'] : null, $row['Date_Debut'], $row['Date_Fin'], $row['Raison'], $row['Date_Creation']);
+    }
+}
